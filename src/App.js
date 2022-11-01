62a17fc619605653a1964f4f86eebc726c3bc48a
Vivi Schulz <haweirh@gmail.com>
2022-11-01 15:11:16 -0700

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "../src/app.scss"
 import Component from './components/component/Component';
+import Form from "./components/form/Form";
 
 function App() {
   return (
     <div className="App">
     Hello world
       <Component />
+      <Form />
     </div>
   );
 }
