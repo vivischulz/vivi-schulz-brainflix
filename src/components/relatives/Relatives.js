9a7b008a68bf7082acc15639657353b37e84f4f2
Vivi Schulz <haweirh@gmail.com>
2022-11-03 09:18:41 -0700

    Fixed comment function.

--- src/components/relatives/Relatives.js
+++ src/components/relatives/Relatives.js
@@ -1,53 +1,50 @@
 import "./Relatives.scss";
 
-export default function Relatives({comments}){
-
-return(
+export default function Relatives({ comments }) {
+  return (
     <>
-          <section className="form">
-            <p className="form__comment-count"> 3 comments</p>
-            <div className="form__section">
-              <div className="form__form-image"></div>
-              <form className="form__form">
-                <h2 className="form__title">Join the Conversation</h2>
-                <label htmlFor="textArea">COMMENT</label>
-                <textarea
-                  id="textArea"
-                  name="textArea"
-                  placeholder="Add a new comment"
-                  minLength="3"
-                  maxLength="300"
-                ></textarea>
+      <section className="form">
+        <p className="form__comment-count"> 3 comments</p>
+        <div className="form__section">
+          <div className="form__form-image"></div>
+          <form className="form__form">
+            <h2 className="form__title">Join the Conversation</h2>
+            <label htmlFor="textArea">COMMENT</label>
+            <textarea
+              id="textArea"
+              name="textArea"
+              placeholder="Add a new comment"
+              minLength="3"
+              maxLength="300"
+            ></textarea>
 
-                <button className="form__btn">
-                  <span className="form__btn-icon"></span>
-                  <span className="form__btn-name">comment</span>
-                </button>
-              </form>
+            <button className="form__btn">
+              <span className="form__btn-icon"></span>
+              <span className="form__btn-name">comment</span>
+            </button>
+          </form>
+        </div>
+      </section>
+      {comments.length > 0 ?(
+      <section className="comment comment--message-block">
+        {comments.map((comment) => (
+          <article className="comment__section" key={comment.id}>
+            <div className="comment__message-wraper">
+              <div className="comment__message-image"></div>
             </div>
-          </section>
-
-          <section className="comment comment--message-block">
-            { comments.length >0 ? comments.map((comment) => (
-            <article className="comment__section" key={comment.id}>
-             
-              <div className="comment__message-wraper">
-                <div className="comment__message-image"></div>
-              </div>
-
-                <section className="comment__message" >
-                <p className="comment__message-user">{comment.name}</p>
-                <p className="comment__message-date">{comment.timestamp}</p>
-                <p className="comment__message-content">{comment.comment}</p>
-                </section>
-              )):( 
-                <section className="comment__no-data-container">
-                  <p className="comment__no-data">No Comments</p>
-                </section>
-              )
-            </article>
-          </section>
-            }
+            <section className="comment__message">
+              <p className="comment__message-user">{comment.name}</p>
+              <p className="comment__message-date">{comment.timestamp}</p>
+              <p className="comment__message-content">{comment.comment}</p>
+            </section>
+          </article>
+        ))}
+      </section>
+      ):(
+      <section className="comment__no-data-container">
+        <p className="comment__no-data">No Comments</p>
+      </section>
+      )}
     </>
   );
 }
