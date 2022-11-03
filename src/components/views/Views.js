303a5f52cde80e2ce8ca75449a8c6ae1299179ce
Vivi Schulz <haweirh@gmail.com>
2022-11-03 12:11:46 -0700

    Finish part of views layout

--- src/components/views/Views.js
+++ src/components/views/Views.js
@@ -2,24 +2,28 @@ import "./Views.scss";
 
 
 export default function Views({video}){
-    const {title, channel, timestamp, description, ...rest} = video;
+    const {title, channel, timestamp, description, views, likes, ...rest} = video;
     return (
       <>
         <section className="views">
           <h1 className="views__title">{title}</h1>
-          <section>
-            <div>
+          <section className="views__detail">
+            <div className="views__detail--left">
               <p className="views__author">By {channel}</p>
               <p className="views__date">{timestamp}</p>
             </div>
-            <div>
-              <span className="views__icon-views"></span>
-              <span className="views__icon-likes"></span>
+            <div className="views__detail--right">
+              <div class="views__views">
+                <span className="views__icon views__icon-views"></span>
+                <sapn className="views__word">{views}</sapn>
+              </div>
+              <div class="views__likes">
+                <span className="views__icon views__icon-likes"></span>
+                <sapn className="views__word">{likes}</sapn>
+              </div>
             </div>
-            <p className="views__p">
-             {description}
-            </p>
           </section>
+          <p className="views__p">{description}</p>
         </section>
       </>
     );
