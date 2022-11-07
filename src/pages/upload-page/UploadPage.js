81a9e6cc02e37f11e235bf13e3a02927234f8973
Vivi Schulz <haweirh@gmail.com>
2022-11-07 00:21:08 -0800

    create upload video page

--- src/pages/upload-page/UploadPage.js
+++ src/pages/upload-page/UploadPage.js
@@ -1,9 +1,48 @@
 import "./UploadPage.scss";
 
-export default function UploadPage (){
+export default function UploadPage() {
+  return (
+    <>
+      <h1>Upload Video</h1>
+      <section className="upload__section">
+        <article className="upload__thumbnail">
+            <h2 className="upload__title">Video Thumbnail</h2>
+            <img src="#" alt="#"></img>
+        </article>
+        <section className="upload__user-input">
+          <h2 className="upload__title">Title Your Video</h2>
+          <label htmlFor="titleViedeo">
+            <input
+              id="titleViedeo"
+              name="titleViedeo"
+              placeholder="Add a title to your video"
+              minLength="3"
+              maxLength="300"
+              className="upload__textarea"
+            ></input>
+          </label>
+          <h2 className="upload__title">Add a video description</h2>
+          <label htmlFor="textArea">
+            <input
+              id="textArea"
+              name="textArea"
+              placeholder="Add a description to your video"
+              minLength="3"
+              maxLength="300"
+              className="upload__textarea upload__textarea--more-height"
+            ></input>
+          </label>
+        </section>
+      </section>
 
-    return(
-        <p>This is upload page.</p>
-    );
+      <button className="upload__btn">
+        <span className="upload__btn-icon"></span>
+        <span className="upload__btn-name">Publish</span>
+      </button>
+
+      <button className="upload__btn">
+        <span className="upload__btn-name">Cancel</span>
+      </button>
+    </>
+  );
 }
-
