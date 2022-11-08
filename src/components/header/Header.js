91a494426375985c12973db5fdf438c7046b5b9c
Vivi Schulz <haweirh@gmail.com>
2022-11-07 18:10:23 -0800

    Finish upload page css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,36 +7,39 @@ export default function Header () {
     return (
       <>
         <section className="header">
-  
-        <Link to="/">
-            <div className="header__logo" href="/">BrainFlix</div>
-        </Link>
+          <Link to="/">
+            <div className="header__logo" href="/">
+              BrainFlix
+            </div>
+          </Link>
 
           {/*---search group---*/}
           <section className="header__search-wrap">
-              <div className="header__search">
-                <div className="header__search-group">
-                  <span className="header__search-icon"></span>
-                  <input
-                    type="text"
-                    className="header__search-name"
-                    placeholder="Search"
-                    name="search"
-                    autoFocus
-                  />
-                </div>
-                <div className="header__avatar1"></div>
+            <div className="header__search">
+              <div className="header__search-group">
+                <span className="header__search-icon"></span>
+                <input
+                  type="text"
+                  className="header__search-name"
+                  placeholder="Search"
+                  name="search"
+                  autoFocus
+                />
               </div>
-              {/*---button group---*/}
-             
-                <Link to="upload">
-                  <button className="header__btn">
-                    <span className="header__btn-icon"></span>
-                    <span className="header__btn-name">Upload</span>
-                  </button>
-                </Link>
-            
-              <div className="header__avatar2"></div>
+              <div className="header__avatar1"></div>
+            </div>
+            {/*---button group---*/}
+
+            <Link to="upload">
+              <button className="header__btn">
+                <span className="header__btn-icon"></span>
+                <span className="header__btn-name header__btn-name--with-icon">
+                  Upload
+                </span>
+              </button>
+            </Link>
+
+            <div className="header__avatar2"></div>
           </section>
         </section>
       </>
--- src/pages/upload-page/UploadPage.js
+++ src/pages/upload-page/UploadPage.js
@@ -3,46 +3,50 @@ import "./UploadPage.scss";
 export default function UploadPage() {
   return (
     <>
-      <h1>Upload Video</h1>
-      <section className="upload__section">
-        <article className="upload__thumbnail">
-            <h2 className="upload__title">Video Thumbnail</h2>
-            <img src="#" alt="#"></img>
-        </article>
-        <section className="upload__user-input">
-          <h2 className="upload__title">Title Your Video</h2>
-          <label htmlFor="titleViedeo">
-            <input
-              id="titleViedeo"
-              name="titleViedeo"
-              placeholder="Add a title to your video"
-              minLength="3"
-              maxLength="300"
-              className="upload__textarea"
-            ></input>
-          </label>
-          <h2 className="upload__title">Add a video description</h2>
-          <label htmlFor="textArea">
-            <input
-              id="textArea"
-              name="textArea"
-              placeholder="Add a description to your video"
-              minLength="3"
-              maxLength="300"
-              className="upload__textarea upload__textarea--more-height"
-            ></input>
-          </label>
+      <section className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+        <section className="upload__section">
+          <article className="upload__thumbnail">
+            <h2 className="upload__subtitle">Video Thumbnail</h2>
+            <div className="upload__image"></div>
+          </article>
+          <section className="upload__user-input">
+            <h2 className="upload__subtitle">Title Your Video</h2>
+            <label htmlFor="titleViedeo">
+              <input
+                id="titleViedeo"
+                name="titleViedeo"
+                placeholder="Add a title to your video"
+                minLength="3"
+                maxLength="300"
+                className="upload__textarea"
+              ></input>
+            </label>
+            <h2 className="upload__subtitle">Add a video description</h2>
+            <label htmlFor="textArea">
+              <input
+                id="textArea"
+                name="textArea"
+                placeholder="Add a description to your video"
+                minLength="3"
+                maxLength="300"
+                className="upload__textarea upload__textarea--more-height"
+              ></input>
+            </label>
+          </section>
+        </section>
+        <section className="upload__btn-group ">
+          <button className="upload__btn upload__btn--publish ">
+            <span className="upload__btn-icon"></span>
+            <span className="upload__btn-name upload__btn-name--with-icon">
+              Publish
+            </span>
+          </button>
+          <button className="upload__btn upload__btn--cancel">
+            <span className="upload__btn-name">Cancel</span>
+          </button>
         </section>
       </section>
-
-      <button className="upload__btn">
-        <span className="upload__btn-icon"></span>
-        <span className="upload__btn-name">Publish</span>
-      </button>
-
-      <button className="upload__btn">
-        <span className="upload__btn-name">Cancel</span>
-      </button>
     </>
   );
 }
